b1c29212db3ac2b1a18567a2077d51f90f0c81b0
melalex <alexander.melashchenko@gmail.com>
2025-03-03 02:48:21 +0200

    Add lab 2

--- src/util/metrics.py
+++ src/util/metrics.py
@@ -1,9 +1,17 @@
 from sklearn import metrics
 
 
-def calculate_metrics(y_test, y_pred):
+def calculate_regression_metrics(y_test, y_pred):
     return {
         "r2": metrics.r2_score(y_test, y_pred),
         "mae": metrics.mean_absolute_error(y_test, y_pred),
         "mse": metrics.mean_squared_error(y_test, y_pred),
+    }
+
+
+def calculate_classification_metrics(y_test, y_pred):
+    return {
+        "f1": metrics.f1_score(y_test, y_pred, average="weighted"),
+        "accuracy": metrics.accuracy_score(y_test, y_pred),
+        "recall": metrics.recall_score(y_test, y_pred, average="weighted"),
     }
