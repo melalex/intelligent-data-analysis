4f51be8e202006dbed96ff111d761ce67c4c94f2
melalex <alexander.melashchenko@gmail.com>
2025-03-10 16:59:37 +0200

    Add lab 4

--- src/visualization/plot.py
+++ src/visualization/plot.py
@@ -9,3 +9,23 @@ def plot_feature_count_to_f1(x, y, size=(12, 6)):
     plt.ylabel("F-score")
     plt.legend()
     plt.show()
+
+
+def plot_2d_data(x, y, size=(12, 6)):
+    plt.figure(figsize=size)
+    scatter = plt.scatter(x[:, 0], x[:, 1], c=y, cmap="viridis", edgecolors="k")
+    plt.colorbar(scatter, label="Class Label")
+    plt.xlabel("Feature 1")
+    plt.ylabel("Feature 2")
+    plt.title("2D Data Visualization with Colors")
+    plt.show()
+
+
+def plot_mean_shift_cluster_score(bandwidth, metric, metric_name, size=(12, 6)):
+    plt.figure(figsize=size)
+    plt.plot(bandwidth, metric, label=metric_name)
+    plt.title(metric_name)
+    plt.xlabel("Bandwidth")
+    plt.ylabel(metric_name)
+    plt.legend()
+    plt.show()
